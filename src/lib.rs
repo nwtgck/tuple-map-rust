be6e7583e23bc3cf9d1e50358a16e94dbbe7700e
Ryo Ota <nwtgck@nwtgck.org>
2020-05-18 19:45:45 +0900

    test: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,35 @@ macro_rules! tuple_map {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn simple_test() {
+        let a = tuple_map!((10, "hello", 1.23), x, {
+            format!("{:?}", x)
+        });
+        assert_eq!(a, ("10".to_string(), "\"hello\"".to_string(), "1.23".to_string()));
+    }
+
+    #[test]
+    fn test2() {
+        fn f1(vec: &mut Vec<i32>) -> u32 {
+            vec.push(1);
+            10
+        }
+
+        fn f2(vec: &mut Vec<i32>) -> &'static str {
+            vec.push(2);
+            "hello"
+        }
+
+        fn f3(vec: &mut Vec<i32>) -> f32 {
+            vec.push(3);
+            1.2
+        }
+
+        let mut vec: Vec<i32> = Vec::new();
+        let a = tuple_map!((f1, f2, f3), f, {
+            f(&mut vec)
+        });
+        assert_eq!(a, (10, "hello", 1.2));
+        assert_eq!(vec, vec![1, 2, 3]);
     }
 }
